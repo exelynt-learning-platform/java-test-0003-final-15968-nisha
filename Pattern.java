416df650c4d8cddbbdd9ea0565e3224dbac66046
nishathakur5758-hue <nishathakur5758@gmail.com>
2026-03-14 14:07:04 +0530

    Create Pattern.java
    Implement a pattern printing program that alternates between 1 and 0.

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+public class Pattern {
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+
+             start with 1 or 0
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num);
+                num = (num == 1) ? 0 : 1; 
+            }
+
+            System.out.println();
+        }
+    }
+}
